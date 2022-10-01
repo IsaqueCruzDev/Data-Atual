ec037dcc0455b77f201877d1621ad2909a9f6970
IsaqueCruz06 <oliveiraioc616@gmail.com>
2022-10-01 11:16:22 -0300

    código novo

--- index.js
+++ index.js
@@ -1,122 +1,121 @@
-const dataMostrar = datinha();
-const horarioMostrar = horaAtual();
- 
-const msg = `${dataMostrar} <br/> ${horarioMostrar}`;
+const mostraData = dataGeral();
+const mostraMes = mesGeralEAno();
+const msg = `${mostraData} ${mostraMes}`;
 
-function datinha() {
+function dataGeral() { 
     const data = new Date();
-    const dia = data.getDay();
-    const diaSemanaTexto = data.getDate();
-    const mes = data.getMonth() + 1;
+    const dia = data.getDate();
+    const diaSemana = data.getDay();
+    const mes = data.getMonth();
     const ano = data.getFullYear();
-    const hora = data.getHours();
-    const minuto = data.getMinutes();
-    const segundo = data.getSeconds();
 
-    let diaDaSemana;
-    let mesDoAno;
-    switch (dia) { 
+    let diaSemanaTexto;
+    switch (diaSemana) { 
         case 0:
-            diaDaSemana = 'Domingo';
+            diaSemanaTexto = 'Domingo';
             break;
         case 1:
-            diaDaSemana = 'Segunda-Feira';
+            diaSemanaTexto = 'Segunda-Feira';
             break;
         case 2:
-            diaDaSemana = 'Terça-Feira';
+            diaSemanaTexto = 'Terça-Feira';
             break;
         case 3:
-            diaDaSemana = 'Quarta-Feira';
+            diaSemanaTexto = 'Quarta-Feira';
             break;
         case 4:
-            diaDaSemana = 'Quita-Feira';
+            diaSemanaTexto = 'Quinta-Feira';
             break;
         case 5:
-            diaDaSemana = 'Sexta-Feira';
+            diaSemanaTexto = 'Sexta-Feira';
             break;
         case 6:
-            diaDaSemana = 'Sábado';
+            diaSemanaTexto = 'Sábado';
             break;
-        
         default:
-            diaDaSemana = '';
+            diaSemanaTexto = '';
+            break;
     }
-
-    switch (mes) { 
-        case 1:
-        mesDoAno = 'Janeiro';
-            break;
-        
-        case 2:
-        mesDoAno = 'Fevereiro';
-            break;
-        
-        case 3:
-        mesDoAno = 'Março';
-            break;
-        
-        case 4:
-        mesDoAno = 'Abril';
-            break;
-        
-        case 5:
-        mesDoAno = 'Maio';
-            break;
-        
-        case 6:
-        mesDoAno = 'Junho';
-            break;
-        case 7:
-        mesDoAno = 'Julho';
-            break;
-        case 8:
-        mesDoAno = 'Agosto';
-            break;
-        
-        case 9:
-        mesDoAno = 'Setembro';
-            break;
-        
-        case 10:
-        mesDoAno = 'Outubro';
-            break;
-        
-        case 12:
-        mesDoAno = 'Novembro';
-            break;
-        
-        case 12:
-        mesDoAno = 'Dezebro';
-            break;
-        
-        default:
-            mesDoAno = '';
-    }
-    
-    const msgData = `${diaDaSemana}, ${dia} de ${mesDoAno} de ${ano}`;
-    return msgData;
-}
-
-function horaAtual() { 
-    const data = new Date();
-    const hora = data.getHours();
-    const minuto = data.getMinutes();
-    const segundo = data.getSeconds();
-    const msg = `${hora}:${minuto}:${segundo}`;
+    const msg = `${diaSemanaTexto}, ${dia}`;
     return msg;
 }
 
-function criaElemento() { 
-    const elemento = document.createElement('element');
+console.log(dataGeral());
+
+
+function mesGeralEAno() { 
+    const data = new Date();
+    const mes = data.getMonth() + 1;
+    const ano = data.getFullYear();
+    
+    function retornaValor(num) {
+        return num >= 10 ? num : `0${num}`;
+    }
+    
+    const hora = retornaValor(data.getHours());
+    const minuto = retornaValor(data.getMinutes());
+    const segundo = retornaValor(data.getSeconds());
+
+
+    let mesRetorno;
+    switch (mes) { 
+        case 1:
+            mesRetorno = 'Janeiro';
+            break;
+        case 2:
+            mesRetorno = 'Fevereiro';
+            break;
+        case 3:
+            mesRetorno = 'Março';
+            break;
+        case 4:
+            mesRetorno = 'Abril';
+            break;
+        case 5:
+            mesRetorno = 'Maio';
+            break;
+        case 6:
+            mesRetorno = 'Junho';
+            break;
+        case 7:
+            mesRetorno = 'Julho';
+            break;
+        case 8:
+            mesRetorno = 'Agosto';
+            break;
+        case 9:
+            mesRetorno = 'Setembro';
+            break;
+        case 10:
+            mesRetorno = 'Outubro';
+            break;
+        case 11:
+            mesRetorno = 'Novembro';
+            break;
+        case 12:
+            mesRetorno = 'Dezembro';
+            break;
+        default:
+            mesRetorno = '';
+            break;
+    }
+
+    const msg = `de ${mesRetorno} de ${ano} <br /> ${hora}:${minuto}:${segundo}`
+    return msg;
+}
+
+
+function criaElemento() {
+    const elemento = document.createElement('elemento');
     return elemento;
 }
 
-function resultadoEscopo() { 
-    const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = "";
+function resultadoData () { 
+    const h1 = document.querySelector('div #conteiner h1');
+    h1.innerHTML = "";
     const elemento = criaElemento();
     elemento.innerHTML = msg;
     resultado.appendChild(elemento);
 }
 
-resultadoEscopo();
+resultadoData();
